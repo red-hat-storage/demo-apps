6b21bc7841d2d918ae7f6da19a204b4a45180b41
Daniel Schimpfoessl <daniel@schimpfoessl.com>
2021-02-19 09:04:05 -0600

    Remove intermediate image for python-app

--- ocs.sh
+++ ocs.sh
@@ -8,14 +8,11 @@ ___ "Cleanup existing environment"
 cd $home/photo-album; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
 oc delete -f app.yaml
 oc delete bc photo-album -n demo
-oc delete bc python-app -n demo
 ___ "Import dependencies and create build config"
 oc import-image ubi8/python-38 --from=registry.redhat.io/ubi8/python-38 --confirm -n demo
 __ " * Deploy application"
 oc create -f app.yaml
 __ " * Build the application image"
-oc new-build --binary --strategy=docker --name python-app -n demo
-oc start-build python-app --from-file python-app.Dockerfile -F -n demo
 oc new-build --binary --strategy=docker --name photo-album -n demo
 oc start-build photo-album --from-dir . -F -n demo
 cd $home
