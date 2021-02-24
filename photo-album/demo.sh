f3dac0ed70391469aee963b6486d3ae1bac9217d
Daniel Schimpfoessl <daniel@schimpfoessl.com>
2021-02-24 11:08:54 -0600

    Move demo script into demo folder.

--- photo-album/demo.sh
+++ photo-album/demo.sh
@@ -1,11 +1,11 @@
 home=$( cd "$(dirname "$0")" ; pwd -P )
-source $home/format.sh $@
+source $home/../format.sh $@
 oc project demo > /dev/null 2>&1
-oc apply -f $home/namespace.yaml
+oc apply -f $home/../namespace.yaml
 
 __ "Object Bucket Demo"
 ___ "Cleanup existing environment"
-cd $home/photo-album; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
+cd $home; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
 oc delete -f app.yaml
 oc delete bc photo-album -n demo
 ___ "Import dependencies and create build config"
@@ -15,4 +15,4 @@ oc create -f app.yaml
 __ " * Build the application image"
 oc new-build --binary --strategy=docker --name photo-album -n demo
 oc start-build photo-album --from-dir . -F -n demo
-cd $home
+cd -
