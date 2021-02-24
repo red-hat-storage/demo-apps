9cc33a9246500fa90ac261b7d23d50e63cb5d95c
Daniel Schimpfoessl <daniel@schimpfoessl.com>
2021-02-24 13:12:25 -0600

    Missing resources is success for delete.

--- photo-album/demo.sh
+++ photo-album/demo.sh
@@ -1,11 +1,11 @@
 demo_home=$( cd "$(dirname "$([[ $0 != $BASH_SOURCE ]] && echo "$BASH_SOURCE" || echo "$0" )")" ; pwd -P )
 source $demo_home/../format.sh $@
+cd $demo_home; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
 
 __ "Object Bucket Demo"
 ___ "Cleanup existing environment"
-cd $demo_home; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
-oc delete -f app.yaml
-oc delete bc photo-album -n demo
+oc delete --ignore-not-found=1 -f app.yaml
+oc delete --ignore-not-found=1 bc photo-album -n demo
 ___ "Import dependencies and create build config"
 oc import-image ubi8/python-38 --from=registry.redhat.io/ubi8/python-38 --confirm -n demo
 __ " * Deploy application"
