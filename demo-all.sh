97b26817492acb01debf297164f6b0b390f28ed8
Daniel Schimpfoessl <daniel@schimpfoessl.com>
2021-02-27 14:56:47 -0600

    Adding minecraft-server demo after testing.

--- demo-all.sh
+++ demo-all.sh
@@ -2,3 +2,4 @@ home=$( cd "$(dirname "$0")" ; pwd -P )
 source $home/format.sh $@
 
 . $home/photo-album/demo.sh $@
+. $home/minecraft-server/demo.sh $@
---
+++ minecraft-server/demo.sh
@@ -0,0 +1,10 @@
+demo_home=$( cd "$(dirname "$([[ $0 != $BASH_SOURCE ]] && echo "$BASH_SOURCE" || echo "$0" )")" ; pwd -P )
+source $demo_home/../format.sh $@
+cd $demo_home; perl -pe "s/\{+domain\}+/$domain/g" app.yaml.tmpl > app.yaml 
+
+__ "Block Storage Demo"
+___ "Cleanup existing environment"
+oc delete --ignore-not-found=1 -f app.yaml
+__ " * Deploy application"
+oc create -f app.yaml
+cd -
